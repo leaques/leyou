523c443921734e71095734cfce00ab27e5943635
leaques <leaques@163.com>
2019-08-03 22:33:15 +0800

    leyou first commit

---
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -0,0 +1,20 @@
+package com.leyou.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum ExceptionEnum {
+
+    PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
+    CATEGORY_NOT_FOND(404, "商品分类没查到"),
+
+    ;
+    private int code;
+    private String msg;
+
+    //枚举具有固定实例个数的类
+}
